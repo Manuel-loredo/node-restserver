a4f9a2fae8b41592f94a185b623d3ed43c4f2b8e
Jesus Manuel Loredo Sauceda <manuel.lorsau@gmail.com>
2020-09-25 21:22:38 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 //Puerto
 //==============================
 process.env.PORT = process.env.PORT || 3000;
+
+//=============================
+//Entorno
+//==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============================
+//Base de Datos
+//==============================
+
+let urlDB;
+//if(process.env.NODE_ENV === 'dev'){
+   // urlDB = 'mongodb://localhost:27017/cafe';
+//}else {
+    urlDB = 'mongodb+srv://manuel-loredo:eLPVAIpCfbA3mTcR@cluster0.nuhxh.mongodb.net/cafe';
+//}
+
+process.env.URLDB = urlDB;
+//mongodb+srv://manuel-loredo:eLPVAIpCfbA3mTcR@cluster0.nuhxh.mongodb.net/cafe
+//mongodb://localhost:27017/cafe
